3685fcc6ef4e91d1a4f103938047a635adf05dcc
Tycho887 <michaelrj03@gmail.com>
2023-10-27 14:11:49 +0200

    yas

--- oblig5.py
+++ oblig5.py
@@ -11,18 +11,10 @@ def updateSIR(s_y, i_y, r_y, b, k):
     r_new = r_y + k*i_y
     return s_new, i_new, r_new
 
-s, i ,r = updateSIR(s, i, r, b, k)
-
-data = {"day":[0], "s":[s], "i":[i], "r":[r]}
-
-for i in range(1,121):
-    s_new, i_new, r_new = updateSIR(data["s"][-1], data["i"][-1], data["r"][-1], b, k)
-    data["day"].append(i+1)
-    data["s"].append(s_new)
-    data["i"].append(i_new)
-    data["r"].append(r_new)
-
-with open("data/pan2.csv","w") as outfile:
-    outfile.write("day,susceptible,infected,recovered\n")
+with open("data/pan2.csv","w") as output:
+    output.write("day,susceptible,infected,recovered\n")
+    s_in, i_in, r_in = s, i, r
     for i in range(120):
-        outfile.write(f"{data['day'][i]},{data['s'][i]:.4e},{data['i'][i]:.4e},{data['r'][i]:.4e}\n")
+        s_in, i_in, r_in = updateSIR(s_in, i_in, r_in, b, k)
+        print(s_in, i_in, r_in)
+        output.write(f"{i+1},{s_in:.4e},{i_in:.4e},{r_in:.4e}\n")
